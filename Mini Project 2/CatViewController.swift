8cb1d472cd6e1dbe062a9ba2ae8368f99a3c3623
Neferrtari Joes <nefertarijones5@gmail.com>
2022-06-28 11:34:46 -0400

    Initial Commit

---
+++ Mini Project 2/CatViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CatViewController.swift
+//  Mini Project 2
+//
+//  Created by Nefertari Jones on 6/24/22.
+//
+
+import UIKit
+
+class CatViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Mini Project 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mini Project 2
+//
+//  Created by Nefertari Jones on 6/23/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
